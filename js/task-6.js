84c7433047ca67f0a1b4b07e4ea0b556378bf3bf
Kseniia <kkorcmaruk@gmail.com>
2024-12-25 23:27:24 +0100

    hw 7

--- js/task-6.js
+++ js/task-6.js
@@ -1,44 +1,44 @@
-const input = document.querySelector('input');
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
+const controls = document.querySelector("#controls");
+const input = controls.querySelector("input");
+const createButton = controls.querySelector("[data-create]");
+const destroyButton = controls.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
 function createBoxes(amount) {
+  destroyBoxes();
+
   const boxes = [];
+  let size = 30;
 
   for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    const size = 30 + i * 10;  
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
+    const box = document.createElement("div"); 
+    box.style.width = `${size}px`; 
+    box.style.height = `${size}px`; 
     box.style.backgroundColor = getRandomHexColor(); 
-
-    boxes.push(box);
+    box.style.margin = "5px";
+    size += 10; 
+    boxes.push(box); 
   }
 
   boxesContainer.append(...boxes);
 }
 
 function destroyBoxes() {
-  boxesContainer.innerHTML = '';
+  boxesContainer.innerHTML = ""; 
 }
 
-createButton.addEventListener('click', () => {
-  const amount = parseInt(input.value, 10);
-
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value); 
   if (amount >= 1 && amount <= 100) {
     createBoxes(amount); 
+    input.value = ""; 
+  } else {
+    alert("Please enter a number between 1 and 100."); 
   }
-
-  input.value = '';
 });
 
-destroyButton.addEventListener('click', () => {
-  destroyBoxes(); 
-});
+destroyButton.addEventListener("click", destroyBoxes);
